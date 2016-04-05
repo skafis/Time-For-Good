f4b4c2dbf84042cf341d81834708c88aedab8cff
Time for Good <timefor4good@gmail.com>
2016-04-05 10:54:48 +0000

    Commented hard to understand sections

--- project/src/.~c9_invoke_Ieibbl.py
+++ project/src/.~c9_invoke_Ieibbl.py
@@ -6,21 +6,17 @@ from social.backends.facebook import FacebookOAuth2 as facebook
 from django.contrib.auth.models import User
 
 # Create your tests here.
-# Testcase for 3rd party logins data pipeline(pipeline.py)
+# Test for
 class ThirdPartyLoginPipelineTest(TestCase):
     def setUp(self):
-        # User object keyword arguments
     	user_kwargs=dict({    		
     		'first_name':"name1", 
     		'last_name':"name2", 
     		})
-        # Create user object
     	user = User.objects.create_user(
     		username="name1", 
     		**user_kwargs
     		)
-    
-        # Arguments used to test the function		
         self.arg1 = DjangoStrategy
         self.arg2 = ('dgdj', 'gugkl', 'ghfhj')
         self.arg3 = dict({
@@ -37,7 +33,6 @@ class ThirdPartyLoginPipelineTest(TestCase):
     	print self.arg2
     	print self.arg3
     	print "End of arguments."
-        # function being tested
         result = update_user_social_data(self.arg1, *self.arg2, **self.arg3)
 
         self.assertTrue(result)
--- project/src/pipeline.py
+++ project/src/pipeline.py
@@ -5,14 +5,18 @@ def update_user_social_data(strategy, *args, **kwargs):
     """Set the name and avatar for a user only if is new.
     """
     print 'update_user_social_data ::', strategy
+    # Testing for if a new user is logging in
     if not kwargs['is_new']:
         return
 
     full_name = ''
+    # Value of backend
     backend = kwargs['backend']
 
+    # User model object
     user = kwargs['user']
 
+    # Different methods of getting user name(depending on backend)
     if ( isinstance(backend, FacebookOAuth2)
     ):
         full_name = kwargs['response'].get('name')
@@ -24,7 +28,10 @@ def update_user_social_data(strategy, *args, **kwargs):
 
     user.full_name = full_name
 
+    # Handling  backends
+    # Involves getting image of user data
     
+    # Facebook backend
     if isinstance(backend, FacebookOAuth2):
         fbuid = kwargs['response']['id']
         image_name = 'fb_avatar_%s.jpg' % fbuid
@@ -36,6 +43,7 @@ def update_user_social_data(strategy, *args, **kwargs):
             ContentFile(image_stream.read()),
         )
     
+    # linkedin backend
     elif isinstance(backend, LinkedinOAuth2):
         if kwargs['response'].get('pictureUrl'):
             image_name = 'linked_avatar_%s.jpg' % full_name
@@ -46,7 +54,8 @@ def update_user_social_data(strategy, *args, **kwargs):
                 image_name,
                 ContentFile(image_stream.read()),
             )
+    # Update/Create user objects
     user.save()
 
-
+    # Return user object
     return {"user": user}
